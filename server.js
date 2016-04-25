bed5193d76e5b1e2f7144c8e6e3a7f62d714488a
Kamren Zorgdrager <kamrenz@gmail.com>
2016-04-25 10:28:43 -0700

    Added files via upload
    Updating server

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-'use strict';
-
 // Importing express module for our node middleware
 var express = require('express');
 
-var fs = require('fs');
+//Create the "express" server for routing and static files.
+var app = express();
 
-// ------------------------------------------------------------------
-// Configurable variables section...
-// ------------------------------------------------------------------
-// Web server port
-// If 8080 doesn't work try 9080
-var BASE_PORT = 8080;
+//Setting port
+app.set('port', (process.env.PORT || 8080));
+
+//Utilize CORS on express
+//	Enable for all routes
+//app.use(cors());
 
 // Compute the working directory for serving static files
-// makes assumptions about layout of node and directory structure
-// working directories/projects etd.
-var ROOT_DIR = __dirname + '/';
-ROOT_DIR = fs.realpathSync(ROOT_DIR);
-if (!fs.existsSync(ROOT_DIR)) {
-	console.log('Error: cannot find working directory: ' + ROOT_DIR);
-	exit();
-}
-
-/**
- * Create the "express" server for routing and static files.
- **/
-var app = express();
+app.use(express.static(__dirname + '/'));
 
 /**
  * Adds a simple logging, "mounted" on the root path.
@@ -36,10 +23,8 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.use(express.static(ROOT_DIR));
 
-app.listen(BASE_PORT, function() {
-	console.log('Node server started @ http://localhost:' + BASE_PORT);
-	console.log('Serving static files from ' + ROOT_DIR);
+app.listen(app.get('port'), function() {
+	console.log('Node server started @ http://localhost:' + app.get('port'));
 	console.log('Press Ctrl + c for server termination');
 });
